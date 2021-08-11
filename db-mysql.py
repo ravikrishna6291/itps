b8a3dc77442caedb30adac7b4062317adc861909
chch9015 <charankumarraju777@gmail.com>
2021-08-11 14:52:57 +0530

    updated the python code to fetch the databases from mysql-server

--- db-mysql.py
+++ db-mysql.py
@@ -1,2 +1,11 @@
 # sameple programme for python
-print('hello world')
+import mysql.connector
+
+connector = mysql.connector.connect(host="13.127.88.192",database="itps_db", user="itps_db_user", passwd="IjRJnKj2hFknimX7vkUc")
+
+mycursor = connector.cursor()
+
+mycursor.execute("show databases")
+
+for i in mycursor:
+    print(i)
